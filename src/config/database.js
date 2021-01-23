32ddb86f9cfadbb8733331e5b906c5a657ea9eb0
Mateus Rodrigues <mcmateus070@gmail.com>
2021-01-23 16:35:02 -0300

    first version

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = { 
+    dialect: "postgres",
+    host: "localhost",
+    username: "postgres",
+    password: "********",
+    database: "sequelize_test",
+    define: {
+        timestamps: true,
+        underscored: true,
+    },
+};
